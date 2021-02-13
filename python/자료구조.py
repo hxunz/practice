2d0ccc70ad64220189f806fdd1d72ccc6f1a5cac
hxunz <guswl2843@gmail.com>
2021-02-13 18:50:26 +0900

    추첨 프로그램 작성
     - random 함수를 사용
    
     - users 라는 list를 생성
    
     - range를 이용해서 숫자를 일일이 적지 않아도 됨
    
     - 그러나 type이 range로 되기 때문에 list에서 사용하는 함수를 쓸 수 없음 그래서 type을 list로 변환
    
     - shuffle을 이용해서 1에서 20까지의 숫자들을 무작위로 배열
    
     - winners라는 변수를 이용해서 users중에서 숫자 4개를 추첨
    
     - 치킨 당첨자는 winners에서 0번째 숫자
    
     - 커피 당첨자는 winners에서 1부터 마지막까지의 숫자
    
     - 따라서 치킨 당첨자와 커피 당첨자는 중복이 되지 않는다

---
+++ python/자료구조.py
@@ -0,0 +1,26 @@
+# Quiz) 당신의 학교에서는 파이썬 대회를 주회합니다.
+# 참석률을 높이기 위해 댓글 이벤트를 진행하기로 하였습니다.
+# 댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰을 받게 됩니다.
+# 추첨 프로그램을 작성하시오.
+
+# 조건 1 : 편의상 댓글은 20명이 작성하였고 아이디는 1~20 이라고 가정
+# 조건 2 : 댓글 내용과 상관 없이 무작위로 추첨하되 중복 불가
+# 조건 3 : random 모듈의 shuffle과 sample을 활용
+
+from random import *
+users = range(1,21) #1부터 20까지 숫자를 생성
+#print(type(users)) type이 range면 list에서만 쓸 수 있는 함수를 사용하지 못함
+users = list(users)
+#print(type(users)) 따라서 type을 list로 바꿔줌
+
+print(users)
+shuffle(users)
+print(users)
+
+winners = sample(users, 4) # 4명 중에서 1명은 치킨, 3명은 커피
+
+print("-- 당첨자 발표 --")
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("커피 당첨자 : {0}".format(winners[1:]))
+print("-- 축하합니다 --")
+
