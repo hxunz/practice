c4677c8b39289de32869cb24aa261a72023dbd5a
hxunz <guswl2843@gmail.com>
2021-03-04 23:13:05 +0900

    모듈과 패키지 예제
     -  새로운 파일을 만든 후 그 파일을 이용하여 내용 출력

---
+++ python/byme.py
@@ -0,0 +1,4 @@
+def sign():
+  print("sns 주소")
+  print("Youtube :  https://www.youtube.com/channel/UCa7OsVbHMN_ZZuesPxg1AVQ")
+  print("Instagram : https://www.instagram.com/hxunzee/")
---
+++ python/모듈예제.py
@@ -0,0 +1,12 @@
+# Quiz) 나만의 시그니처를 남기는 모듈을 만드시오
+
+# 조건 : 모듈 파일명은 byme.py로 작성
+
+# (모듈 사용 예제)
+import byme
+byme.sign()
+
+# (출력 예제)
+# sns 주소
+# Youtube :  https://www.youtube.com/channel/UCa7OsVbHMN_ZZuesPxg1AVQ
+# Instagram : https://www.instagram.com/hxunzee/
