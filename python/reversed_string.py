fc5c8f7f712b0f22b95ec923748bf9cd69837176
hxunz <guswl2843@gmail.com>
2021-08-27 16:57:26 +0900

    reverse string

---
+++ python/reversed_string.py
@@ -0,0 +1,11 @@
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world' = >  'dlrow'
+
+def solution(string):
+    string_reverse = ''  # string 뒤집은 문자열 넣을 공백 설정
+    return (''.join(reversed(string)))  # string을 뒤집어서 ''안에 넣는다
+
+
+def solution(string):
+    return [::-1]
