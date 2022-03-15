93afd99f45bfe055b00c00deed6b2a300bdeaa34
hxunz <guswl2843@gmail.com>
2022-03-15 23:20:16 +0900

    빈배열에 알파벳 넣기

---
+++ Javascript/isograms.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/javascript
+
+function isIsogram(str){
+  const words = [];	
+
+  for (let i = 97; i <= 122; i++) {    
+    words += String.fromCharCode(i);
+}
+}
