ac8e27b080e52dc51e2859f68dcd0ee8310a86d7
hxunz <guswl2843@gmail.com>
2022-03-24 16:45:45 +0900

    중복값이 있을 경우 테스트 코드 통과 못해서 코드 수정

--- Javascript/sum_of_two_lowest_positive_integers.js
+++ Javascript/sum_of_two_lowest_positive_integers.js
@@ -1,14 +1,9 @@
 // https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/javascript
 
 function sumTwoSmallestNumbers(numbers) {  
-  // const number = numbers에서 제일 작은 수
-  const number = Math.min(...numbers);
+  const intNum = numbers.filter(word => word >= 0);
   
-  // const nums = numbers - 제일 작은 수 제외
-  const nums = numbers.filter(word => word !== number);
+  const number = intNum.sort((a, b) => a-b);
   
-  // const num = nextnum에서 제일 작은 수 
-  const num = Math.min(...nums);
-  
-  return number + num
+  return number[0] + number[1];
 }
