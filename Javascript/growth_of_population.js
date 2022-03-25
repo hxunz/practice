eabad5cee8deb580a9d3791b596c60a6934319de
hxunz <guswl2843@gmail.com>
2022-03-25 23:07:36 +0900

    Growth of a Population

---
+++ Javascript/growth_of_population.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/javascript
+
+function nbYear(p0, percent, aug, p) {
+  let year = 0;
+  for (let i=0; p0 <= p; i++){
+    p0 = p0 + p0 * percent/100 + aug;  
+    console.log(p0);
+    year += 1;
+  }
+  return year;
+}
