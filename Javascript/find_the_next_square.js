0d67e7f36738e52e80009c01a8b077f0203789b5
hxunz <guswl2843@gmail.com>
2022-03-27 17:50:02 +0900

    Find the next perfect square!

--- Javascript/find_the_next_square.js
+++ Javascript/find_the_next_square.js
@@ -1,2 +1,11 @@
 // https://www.codewars.com/kata/56269eb78ad2e4ced1000013/train/javascript
 
+function findNextSquare(sq) {
+  const num = Math.sqrt(sq);
+  
+  if (Number.isInteger(num) === false) {
+    return -1;
+  } 
+  
+  return Math.pow(num+1, 2);
+}
