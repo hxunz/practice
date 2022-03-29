ba887fe7f0e5900420145ac29499c06bbae797a6
hxunz <guswl2843@gmail.com>
2022-03-29 16:09:11 +0900

    Multiples of 3 or 5

--- Javascript/multiples_of_3_or_5.js
+++ Javascript/multiples_of_3_or_5.js
@@ -1,2 +1,11 @@
 // https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
 
+function solution(number){
+  let num = 0;
+  for (i=1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      num += i;
+    }
+  }
+  return num;
+}
