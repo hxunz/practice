d03c84d3a16d9af368853d5de1098304e492eaeb
hxunz <guswl2843@gmail.com>
2022-03-31 17:47:07 +0900

    ♻️ A-B 차집합을 구하는 문제여서 B-A 를 구하는 코드를 삭제했다.

--- Javascript/array_diff.js
+++ Javascript/array_diff.js
@@ -1,11 +1,5 @@
 // https://www.codewars.com/kata/523f5d21c841566fde000009/train/javascript
 
 function arrayDiff(a, b) {
-  const diff = a.filter(x => !b.includes(x)).concat(b.filter(x => !a.includes(x)));
-  
-  if (a.length === 0 | b.length === 0) {
-    return a
-  } else {
-    return diff;
-  }
+  return a.filter(x => !b.includes(x));
 }
