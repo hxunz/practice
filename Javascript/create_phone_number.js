754a24110bc4fa5e37d98bd85bd1dbcdca1ed58d
hxunz <guswl2843@gmail.com>
2022-04-02 21:23:23 +0900

    Create Phone Number

---
+++ Javascript/create_phone_number.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+
+function createPhoneNumber(numbers){
+  return '(' + numbers[0] + numbers[1] + numbers[2] + ') ' + numbers[3] + numbers[4] + numbers[5] + '-' + numbers[6] + numbers[7] + numbers[8] + numbers[9]
+}
