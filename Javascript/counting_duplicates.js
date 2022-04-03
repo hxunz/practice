3863e5ebe53e2b25275bb8632309b69be1004e3e
hxunz <guswl2843@gmail.com>
2022-04-03 22:23:26 +0900

    Counting Duplicates

---
+++ Javascript/counting_duplicates.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/javascript
+
+function duplicateCount(text){
+  const arrText = [...text];
+  const lowerText = arrText.map(num => num.toLowerCase());
+  
+  let countNum = lowerText.reduce(function (allNumbers, number) {
+    if (number in allNumbers) {
+      allNumbers[number]++;
+    } else {
+      allNumbers[number] = 1;
+    }
+    return allNumbers;
+  }, {});
+  
+  const result = Object.values(countNum).filter(num => num > 1);
+  
+  return result.length === 0 ? 0 : result.length;
+}
