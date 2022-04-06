703d9778ce4dbf1881447d6987a4349f9cb01849
hxunz <guswl2843@gmail.com>
2022-04-06 21:54:15 +0900

    Replace With Alphabet Position

--- Javascript/replace_with_alphabet_position.js
+++ Javascript/replace_with_alphabet_position.js
@@ -1,1 +1,7 @@
 // https://www.codewars.com/kata/546f922b54af40e1e90001da
+
+function alphabetPosition(text) {
+  const words = text.toLowerCase().replace(/[^a-z]/g, '');
+  const alphabet = [...words].map(w=> w.charCodeAt(0) - 97+1);
+  return alphabet.join(' ');
+}
