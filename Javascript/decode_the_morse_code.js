8c1ef0e56db0bd5441dccf89b23cf4c4f492273b
hxunz <guswl2843@gmail.com>
2022-04-08 15:24:45 +0900

    Decode the Morse code

---
+++ Javascript/decode_the_morse_code.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/54b724efac3d5402db00065e/train/javascript
+
+decodeMorse = function (morseCode) {
+  const code = morseCode.split("   ");
+  const alphabet = code.map(word => word.split(' ').map(w => MORSE_CODE[w]).join(''));
+  const words = alphabet.join(' ').trim();
+  
+  return words.toUpperCase();
+}
