a79914a727d2483573e3b52a69624f41e2e31955
hxunz <guswl2843@gmail.com>
2022-04-14 21:17:51 +0900

    Moving Zeros To The End

--- Javascript/moving_zeros_to_the_end.js
+++ Javascript/moving_zeros_to_the_end.js
@@ -1,2 +1,13 @@
 // https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
 
+function moveZeros(arr) {
+  let array = arr.filter((num) => num !== 0);
+  
+  for (i=0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      array.push(0)
+    }
+  }
+  
+  return array;
+}
