f9424f8c8d40a61cb073f61197346feefb90de44
hxunz <guswl2843@gmail.com>
2022-04-16 20:46:28 +0900

    Valid Parentheses

--- Javascript/valid_parentheses.js
+++ Javascript/valid_parentheses.js
@@ -1,4 +1,19 @@
 // https://www.codewars.com/kata/52774a314c2333f0a7000688/train/javascript
 
+function validParentheses(parens) {
+  const paren = parens.split('');
+  console.log(paren);
+  let map = {
+    '(' : ')',
+    '[' : ']',
+    '{' : '}'
+  }
+  let stack = [];
+  
+  for (i=0; i < paren.length; i++) {
+    if (paren[i] === '(' || paren[i] === '[' || paren[i] === '{') {
+      stack.push(paren[i]);
+    }
+  }
 
-
+}
