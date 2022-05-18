d5490667824e26fd97b5dedb4dff0cbd6054c28c
hxunz <guswl2843@gmail.com>
2022-05-18 23:52:44 +0900

    ✨ 3진법 뒤집기

--- Javascript/programmers/3진법뒤집기.test.js
+++ Javascript/programmers/3진법뒤집기.test.js
@@ -9,12 +9,58 @@
 // 숫자를 뒤집는다
 // 다시 10진법으로 바꾼다.
 
-function solution(n) {
-  const result = parseInt(n.toString(3).split('').reverse().join(''), 3);
-  return result;
+function toDecimal(notation, newNumber) {
+  const reverseNum = [...newNumber].reverse();
+  
+  return reverseNum.reduce((acc, curr, i) => acc + Math.pow(notation, i) * curr, 0);
 }
 
-test('3진법 뒤집기', () => {
-  const n = 45;
-  expect(solution(n)).toBe(7);
-});
+const solution = (notation, number, result = '') => {
+  if (number < notation) {
+    const newNumber = result.split('').reverse().concat(number.toString()).join('');
+    return toDecimal(notation, newNumber);
+  }
+
+  return solution(
+    notation,
+    Math.floor(number / notation),
+    (number % notation) + result
+  )
+}
+
+
+// test('2진법으로 바꾸기', () => {
+//   expect(solution(0)).toBe('0');
+//   expect(solution(1)).toBe('1');
+//   expect(solution(2)).toBe('10');
+//   expect(solution(3)).toBe('11');
+//   expect(solution(4)).toBe('100');
+//   expect(solution(5)).toBe('101');
+//   expect(solution(6)).toBe('110');
+//   expect(solution(7)).toBe('111');
+// });
+
+// test('2진법 뒤집기', () => {
+//   expect(solution(0)).toBe('0');
+//   expect(solution(1)).toBe('1');
+//   expect(solution(2)).toBe('01');
+//   expect(solution(3)).toBe('11');
+//   expect(solution(5)).toBe('101');
+// });
+
+// test('2진법 뒤집기', () => {
+//   expect(solution(0)).toBe(0);
+//   expect(solution(2)).toBe(1);
+//   expect(solution(3)).toBe(3);
+//   expect(solution(5)).toBe(5);
+// });
+
+// test('3진법 뒤집기', () => {
+//   expect(solution(45)).toBe(7);
+//   expect(solution(125)).toBe(229);
+// });
+
+test('N진법 뒤집기', () => {
+  expect(solution(3, 45)).toBe(7);
+  expect(solution(2, 5)).toBe(5);
+})
