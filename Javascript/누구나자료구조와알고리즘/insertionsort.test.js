cac4530cefa876856d7ee9a2d9b3a1c2b4a19f91
hxunz <guswl2843@gmail.com>
2022-06-28 10:08:04 +0900

    ✨ 삽입정렬

---
+++ Javascript/누구나자료구조와알고리즘/insertionsort.test.js
@@ -0,0 +1,15 @@
+const insertionSort = (arr, i=arr.length) => {
+  for (i = 1; i < arr.length; i++) {
+    let currentValue = arr[i];
+    for (j = i - 1; currentValue < arr[j]; j--) {
+      arr[j + 1] = arr[j]
+    }
+    arr[j + 1] = currentValue
+  }
+  return arr
+};
+
+test('insertionSort', () => {
+  expect(insertionSort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
+  expect(insertionSort([4, 3, 2, 5, 1])).toEqual([1, 2, 3, 4, 5]);
+});
