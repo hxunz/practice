2f4209b073dcadb985be164e8d6cae3a2a5ca5e4
hxunz <guswl2843@gmail.com>
2022-06-30 22:59:11 +0900

    ✨ 가운데글자가져오기

---
+++ Javascript/programmers/가운데글자가져오기.test.js
@@ -0,0 +1,11 @@
+const solution = (s) => {
+  const odd = s.length % 2 === 1;
+  const middleWord = Math.floor(s.length / 2);
+
+  return odd ? s[middleWord] : `${s[middleWord - 1]}${s[middleWord]}`;
+};
+
+test('findMiddleWord', () => {
+  expect(solution('abc')).toBe('b');
+  expect(solution('abcd')).toBe('bc');
+})
