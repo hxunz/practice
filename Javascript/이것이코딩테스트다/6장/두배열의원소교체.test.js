b6af481aaefe48ab4fd15b0fdd15782916230991
hxunz <guswl2843@gmail.com>
2022-09-01 15:50:41 +0900

    폴더 이동

---
+++ Javascript/이것이코딩테스트다/6장/두배열의원소교체.test.js
@@ -0,0 +1,10 @@
+const solution = (N, K, A, B) => {
+  // 배열 A의 최소값을 구한다
+  // 배열 B의 최대값을 구한다
+  // 서로 바꾸고 count + 1을 해준다
+  // count 값이 K가 될때까지 반복한다.
+};
+
+test('sumArr', () => {
+  expect(solution(5, 3, [1, 2, 5, 4, 3], [5, 5, 6, 6, 5])).toEqual(26);
+});
