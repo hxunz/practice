988033e0ae85f252e01314a3c0621be8d01a0677
hxunz <guswl2843@gmail.com>
2022-09-05 13:44:30 +0900

    ✨  두개 뽑아서 더하기

--- Javascript/programmers/Level1/두개뽑아서더하기.test.js
+++ Javascript/programmers/Level1/두개뽑아서더하기.test.js
@@ -1,17 +1,18 @@
 const solution = (numbers) => {
   let count = [];
-  
-  for (i = 1; i < numbers.length; i++) {
-    count.push(numbers[0] + numbers[i])
-    count.push(numbers[i]+numbers[i+1])
+
+  for (i = 0; i < numbers.length; i++) {
+    for (j = i + 1; j < numbers.length; j++) {
+      count.push(numbers[i] + numbers[j])
+    }
   }
-  // return [numbers[0] + numbers[1]]
-  // return [numbers[0] + numbers[1], numbers[0] + numbers[2], numbers[1] + numbers[2]]
-  console.log('count::: ', count);
+
+  const set = new Set(count);
+  return [...set].sort((a, b) => a - b);
 };
 
 test('sum', () => {
-  expect(solution([2,1,3,4,1])).toEqual([2,3,4,5,6,7]);
-  // expect(solution([1,2])).toEqual([3]);
-  // expect(solution([1, 2, 3])).toEqual([3, 4, 5]);
+  expect(solution([2, 1, 3, 4, 1])).toEqual([2, 3, 4, 5, 6, 7]);
+  expect(solution([1, 2])).toEqual([3]);
+  expect(solution([1, 2, 3])).toEqual([3, 4, 5]);
 });
