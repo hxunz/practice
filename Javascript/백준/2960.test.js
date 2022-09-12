40c164d43dc198700033336a349001ab6fa6d4d3
hxunz <guswl2843@gmail.com>
2022-09-12 13:56:12 +0900

    ⚡️ Array.from을 써서 정수 배열 만들기

--- Javascript/백준/2960.test.js
+++ Javascript/백준/2960.test.js
@@ -2,14 +2,17 @@
 
 const solution = (n, k) => {
   // 2부터 N까지 모든 정수를 적는.다
-  let numbers = [];
-  for (i = 2; i <= n; i++) {
-    numbers.push(i)
-  }
+  // TODO: Array.from을 써서 해보기
+  let numbers = Array.from({ length: n - 1 }, (v, i) => i + 2);
+
+  // let numbers = [];
+  // for (i = 2; i <= n; i++) {
+  //   numbers.push(i)
+  // }
 
   // 아직 지우지 않은 수 중 가장 작은 수를 찾는다.이것을 P라고 하고, 이 수는 소수이다.
   let p = numbers[0];
-  let count = 0; 
+  let count = 0;
   let prime = [];
 
   // P를 지우고, 아직 지우지 않은 P의 배수를 크기 순서대로 지운다.
@@ -17,11 +20,11 @@ const solution = (n, k) => {
     if (count > k) {
       break;
     }
-
     prime.push(p);
     let num = numbers[0];
     numbers[0] = null;
     count = count + 1;
+
     if (count === k) {
       return num
     }
@@ -43,37 +46,37 @@ const solution = (n, k) => {
 }
 
 test('findPrime', () => {
-  // expect(solution(10, 3)).toEqual(6);
+  expect(solution(10, 3)).toEqual(6);
   // expect(solution(15, 12)).toEqual(7);
   // expect(solution(10, 7)).toEqual(9);
-  expect(solution(2, 1)).toEqual(2);
-  expect(solution(3, 1)).toEqual(2);
-  expect(solution(3, 2)).toEqual(3);
-  expect(solution(4, 1)).toEqual(2);
-  expect(solution(4, 2)).toEqual(4);
-  expect(solution(4, 3)).toEqual(3);
-  expect(solution(5, 1)).toEqual(2);
-  expect(solution(5, 2)).toEqual(4);
-  expect(solution(5, 3)).toEqual(3);
-  expect(solution(5, 4)).toEqual(5);
-  expect(solution(6, 1)).toEqual(2);
-  expect(solution(6, 2)).toEqual(4);
-  expect(solution(6, 3)).toEqual(6);
-  expect(solution(6, 4)).toEqual(3);
-  expect(solution(6, 5)).toEqual(5);
-  expect(solution(7, 1)).toEqual(2);
-  expect(solution(7, 2)).toEqual(4);
-  expect(solution(7, 3)).toEqual(6);
-  expect(solution(7, 4)).toEqual(3);
-  expect(solution(7, 5)).toEqual(5);
-  expect(solution(7, 6)).toEqual(7);
-  expect(solution(8, 1)).toEqual(2);
-  expect(solution(8, 2)).toEqual(4);
-  expect(solution(8, 3)).toEqual(6);
-  expect(solution(8, 4)).toEqual(8);
-  expect(solution(8, 5)).toEqual(3);
-  expect(solution(8, 6)).toEqual(5);
-  expect(solution(8, 7)).toEqual(7);
-  expect(solution(9, 7)).toEqual(5);
-  expect(solution(9, 8)).toEqual(7);
+  // expect(solution(2, 1)).toEqual(2);
+  // expect(solution(3, 1)).toEqual(2);
+  // expect(solution(3, 2)).toEqual(3);
+  // expect(solution(4, 1)).toEqual(2);
+  // expect(solution(4, 2)).toEqual(4);
+  // expect(solution(4, 3)).toEqual(3);
+  // expect(solution(5, 1)).toEqual(2);
+  // expect(solution(5, 2)).toEqual(4);
+  // expect(solution(5, 3)).toEqual(3);
+  // expect(solution(5, 4)).toEqual(5);
+  // expect(solution(6, 1)).toEqual(2);
+  // expect(solution(6, 2)).toEqual(4);
+  // expect(solution(6, 3)).toEqual(6);
+  // expect(solution(6, 4)).toEqual(3);
+  // expect(solution(6, 5)).toEqual(5);
+  // expect(solution(7, 1)).toEqual(2);
+  // expect(solution(7, 2)).toEqual(4);
+  // expect(solution(7, 3)).toEqual(6);
+  // expect(solution(7, 4)).toEqual(3);
+  // expect(solution(7, 5)).toEqual(5);
+  // expect(solution(7, 6)).toEqual(7);
+  // expect(solution(8, 1)).toEqual(2);
+  // expect(solution(8, 2)).toEqual(4);
+  // expect(solution(8, 3)).toEqual(6);
+  // expect(solution(8, 4)).toEqual(8);
+  // expect(solution(8, 5)).toEqual(3);
+  // expect(solution(8, 6)).toEqual(5);
+  // expect(solution(8, 7)).toEqual(7);
+  // expect(solution(9, 7)).toEqual(5);
+  // expect(solution(9, 8)).toEqual(7);
 });
