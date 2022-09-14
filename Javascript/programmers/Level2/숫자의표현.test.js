b5d96ea7c7bd570a410286050780e20a78ef3e67
hxunz <guswl2843@gmail.com>
2022-09-14 16:45:18 +0900

    ✏️ 주석해제

---
+++ Javascript/programmers/Level2/숫자의표현.test.js
@@ -0,0 +1,28 @@
+const solution = (n, count = 0, num = 1) => {
+  if (num > n) {
+    return count
+  }
+  // 1부터 n이 될때까지 더한다.
+  let acc = 0;
+  for (i = num; i <= n; i++) {
+    acc += i
+    if (acc === n) {
+      return solution(n, count += 1, num += 1)
+    } else if (acc > n) {
+      return solution(n, count, num += 1)
+    }
+  }
+  // n을 넘어선다면 count + 1을 해주고 숫자도 + 1을 해준다.
+  // 1번 반복
+  // 숫자가 n이 된다면 카운트 값을 리턴한다.
+}
+
+test('countAcc', () => {
+  expect(solution(15)).toEqual(4);
+  expect(solution(1)).toEqual(1);
+  expect(solution(2)).toEqual(1);
+  expect(solution(3)).toEqual(2);
+  expect(solution(4)).toEqual(1);
+  expect(solution(5)).toEqual(2);
+  expect(solution(6)).toEqual(2);
+});
