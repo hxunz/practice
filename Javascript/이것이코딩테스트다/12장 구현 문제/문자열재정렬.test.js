b545e02832afb8f413ca18d31ae4be7947c43cf9
hxunz <guswl2843@gmail.com>
2022-09-15 13:50:33 +0900

    ⚡️ 문자열 재정렬 다른 풀이

--- Javascript/이것이코딩테스트다/12장 구현 문제/문자열재정렬.test.js
+++ Javascript/이것이코딩테스트다/12장 구현 문제/문자열재정렬.test.js
@@ -1,17 +1,38 @@
-const solution = (S) => {
-  // 주어진 문자열을 쪼개서 배열로 만든다. 
-  const s = S.split('');
+// const solution = (S) => {
+//   // 주어진 문자열을 쪼개서 배열로 만든다. 
+//   const s = S.split('');
 
+//   // 배열을 순회하면서 문자열과 숫자를 나눈다.
+//   let str = [];
+//   let num = [];
+//   s.map((it) => {
+//     if (isNaN(it)) {
+//       return str.push(it)
+//     } else {
+//       return num.push(it)
+//     }
+//   })
+
+//   // 문자열을 오른차순으로 정렬한다.
+//   const sortStr = str.sort().join('');
+
+//   // 숫자를 더한다
+//   const sumNumber = num.map((it) => Number(it)).reduce((acc, cur) => acc + cur)
+//   // 이 둘의 값을 이어서 출력한다.
+//   return sortStr + sumNumber
+// }
+
+const solution = (S) => {
   // 배열을 순회하면서 문자열과 숫자를 나눈다.
   let str = [];
   let num = [];
-  s.map((it) => {
-    if (isNaN(it)) {
-      return str.push(it)
+  for (i = 0; i < S.length; i++) {
+    if (isNaN(S[i])) {
+      str.push(S[i])
     } else {
-      return num.push(it)
+      num.push(S[i])
     }
-  })
+  }
 
   // 문자열을 오른차순으로 정렬한다.
   const sortStr = str.sort().join('');
