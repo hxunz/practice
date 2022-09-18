b55771f0ceac782fdfbcafb9164668b5be9f523b
hxunz <guswl2843@gmail.com>
2022-09-18 19:32:43 +0900

    ⚡️ 예상 대진표 : 재귀

--- Javascript/programmers/Level2/예상대진표.test.js
+++ Javascript/programmers/Level2/예상대진표.test.js
@@ -1,12 +1,19 @@
-const solution = (n, a, b) => {
-  let count = 0;
-  while (a !== b) {
-    a = Math.ceil(a / 2);
-    b = Math.ceil(b / 2);
-    count++;
+// const solution = (n, a, b) => {
+//   let count = 0;
+//   while (a !== b) {
+//     a = Math.ceil(a / 2);
+//     b = Math.ceil(b / 2);
+//     count++;
+//   }
+//   return count;
+// };
+
+const solution = (n, a, b, count = 0) => {
+  if (a === b) {
+    return count;
   }
-  return count;
-};
+  return solution(n, Math.ceil(a / 2), Math.ceil(b / 2), count += 1)
+}
 
 test('', () => {
   expect(solution(8, 4, 7)).toEqual(3);
