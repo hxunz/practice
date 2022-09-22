96fc6bd18c71b9c6a0ab265b2d957ef20967f78c
hxunz <guswl2843@gmail.com>
2022-09-22 13:52:08 +0900

    ✨ 오픈채팅방

--- Javascript/programmers/Level2/오픈채팅방.test.js
+++ Javascript/programmers/Level2/오픈채팅방.test.js
@@ -1,16 +1,25 @@
 const solution = (record) => {
-  let last = {}
-  for (let i = 0; i < record.length; i++) {
-    let id = record[i].split(' ')[1]
-    let name = record[i].split(' ')[2]
-    if (name) {
-      last[id] = name
+  const recordArr = record.map((it) => it.split(' '));
+  let userIds = new Map();
+  let result = [];
+
+  recordArr.map(it => {
+    if (it[0] === "Enter" || it[0] === "Change") {
+      userIds.set(it[1], it[2]);
     }
-  }
-  console.log('last::: ', last);
-  
+  })
+
+  recordArr.map(it => {
+    if (it[0] === 'Enter') {
+      result.push(`${userIds.get(it[1])}님이 들어왔습니다.`);
+    } else if (it[0] === 'Leave') {
+      result.push(`${userIds.get(it[1])}님이 나갔습니다.`);
+    }
+  })
+
+  return result;
 };
 
 test('chat', () => {
-  expect(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])).toEqual(["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]);
+  expect(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"])).toEqual(["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]);
 });
