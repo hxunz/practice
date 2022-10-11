25b59540b5038e10f74a92aac3335931393626e3
hxunz <guswl2843@gmail.com>
2022-10-11 16:21:03 +0900

    ✨ JadenCase 문자열 만들기 다른 방법으로 풀이

--- Javascript/programmers/Level2/JadenCase문자열만들기.test.js
+++ Javascript/programmers/Level2/JadenCase문자열만들기.test.js
@@ -1,12 +1,27 @@
-function solution(s) {
-  let words = s.split(' ');
+// function solution(s) {
+//   let words = s.split(' ');
 
-  const result = words.map((word) => {
-    return word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
-  });
+//   const result = words.map((word) => {
+//     return word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
+//   });
 
-  return result.join(' ');
+//   return result.join(' ');
+// }
+
+
+const solution = (s) => {
+  //주어진 문자열을 모두 소문자로 변경한다.
+  // 공백을 기준으로 나눈다.
+  // 맨 앞글자를 대문자로 바꾼다.
+  //이를 다시 합친다.
+
+  const lowerCase = s.toLowerCase();
+  const strArr = lowerCase.split(' ');
+  const upperCase = strArr.map((it) => it.replace(/^[a-z]/, char => char.toUpperCase()));
+  const result = upperCase.join(' ');
+  return result;
 }
+
 
 test('JadenCase', () => {
   expect(solution("3people unFollowed me")).toEqual("3people Unfollowed Me");
