2b0d303ac8aa7176ee56ae65710e211ee2fb1ec5
hxunz <guswl2843@gmail.com>
2022-10-11 17:53:59 +0900

    ✨ JadenCase 문자열 만들기 다른 풀이 방법

--- Javascript/programmers/Level2/JadenCase문자열만들기.test.js
+++ Javascript/programmers/Level2/JadenCase문자열만들기.test.js
@@ -9,20 +9,40 @@
 // }
 
 
-const solution = (s) => {
-  //주어진 문자열을 모두 소문자로 변경한다.
-  // 공백을 기준으로 나눈다.
-  // 맨 앞글자를 대문자로 바꾼다.
-  //이를 다시 합친다.
+// const solution = (s) => {
+//   //주어진 문자열을 모두 소문자로 변경한다.
+//   // 공백을 기준으로 나눈다.
+//   // 맨 앞글자를 대문자로 바꾼다.
+//   //이를 다시 합친다.
 
-  const lowerCase = s.toLowerCase();
-  const strArr = lowerCase.split(' ');
-  const upperCase = strArr.map((it) => it.replace(/^[a-z]/, str => str.toUpperCase()));
-  const result = upperCase.join(' ');
-  return result;
+//   const lowerCase = s.toLowerCase();
+//   const strArr = lowerCase.split(' ');
+//   const upperCase = strArr.map((it) => it.replace(/^[a-z]/, str => str.toUpperCase()));
+//   const result = upperCase.join(' ');
+//   return result;
+// }
+
+const solution = (s) => {
+  // 문자열을 하나씩 순회한다.
+  // 제일 첫번째 문자열은 무조건 대문자로 바꾸고
+  // 공백 전의 문자들은 소문자로 바꾸고
+  // 공백 이후의 문자 하나는 대문자로 바꾼다.
+  let newStr = '';
+  newStr += (s[0].toUpperCase());
+
+  for (let i = 1; i < s.length; i++) {
+    if (s[i - 1] === ' ' && s[i] !== ' ') {
+      newStr += (s[i].toUpperCase());
+    } else {
+      newStr += (s[i].toLowerCase());
+    }
+  };
+
+  return newStr;
 }
 
 
 test('JadenCase', () => {
   expect(solution("3people unFollowed me")).toEqual("3people Unfollowed Me");
+  expect(solution("a")).toEqual("A");
 });
