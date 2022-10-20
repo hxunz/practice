e8af04f85400f5d6b35b6c25f287888fe2d39539
hxunz <guswl2843@gmail.com>
2022-10-20 16:53:14 +0900

    ✨ 피보나치수 while문 사용

--- Javascript/programmers/Level2/피보나치수.test.js
+++ Javascript/programmers/Level2/피보나치수.test.js
@@ -1,11 +1,24 @@
-const fibonacci = (n) => {
-  let numbers = [0, 1];
-  for (i = 2; i <= n; i++) {
-    numbers.push((numbers[i - 1] % 1234567) + (numbers[i - 2] % 1234567) % 1234567);
-  }
+// const fibonacci = (n) => {
+//   let numbers = [0, 1];
+//   for (i = 2; i <= n; i++) {
+//     numbers.push((numbers[i - 1] % 1234567) + (numbers[i - 2] % 1234567) % 1234567);
+//   }
 
-  return numbers[n] % 1234567;
+//   return numbers[n] % 1234567;
+// };
+
+
+const fibonacci = (n) => {
+  let nums = [0, 1];
+  while (nums.length <= n) {
+    nums.push((nums[nums.length - 2] + nums[nums.length - 1]) % 1234567);
+  }
+  return nums[n] % 1234567;
 };
+
+
+
+
 
 test('fibonacci', () => {
   expect(fibonacci(3)).toEqual(2);
