1459157a715b7469ae5d828f23ca580725a218fe
hxunz <guswl2843@gmail.com>
2023-01-19 19:57:04 +0900

    콜라문제

---
+++ Javascript/programmers/Level1/콜라문제.test.js
@@ -0,0 +1,15 @@
+const solution = (a, b, n, number = 0) => {
+  let count = Math.floor(n / a);
+
+  if (n < a) {
+    return number
+  }
+
+  return solution(a, b, n = n - (count * a) + count * b, number += count * b)
+};
+
+test('solution', () => {
+  expect(solution(2, 1, 20)).toEqual(19);
+  expect(solution(3, 1, 20)).toEqual(9);
+  expect(solution(3, 2, 10)).toEqual(16);
+});
