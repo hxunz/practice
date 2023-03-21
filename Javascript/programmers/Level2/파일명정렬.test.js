1c80c449fe695d615cac2351dad2618b3425a268
hxunz <guswl2843@gmail.com>
2023-03-22 02:48:39 +0900

    파일명 정렬 수정 파일명에 마침표, 빼기 부호, 공백 포함되도록하기

--- Javascript/programmers/Level2/파일명정렬.test.js
+++ Javascript/programmers/Level2/파일명정렬.test.js
@@ -43,7 +43,7 @@ const sorted = (convertFiles) => {
 }
 
 const convert = (file) => {
-  const head = file.match(/^[a-zA-Z]+/)[0];
+  const head = file.match(/^[a-zA-Z.-\s]+/)[0];
   const number = file.match(/\d+/)[0];
   const tail = file.slice(head.length + number.length);
 
@@ -79,5 +79,6 @@ test('convert', () => {
 })
 
 test('solution', () => {
-  expect(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"])).toEqual(["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"])
+  expect(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"])).toEqual(["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]);
+  expect(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"])).toEqual(["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]);
 })
