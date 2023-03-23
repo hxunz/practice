9563e63359b810cf2116a3cc2cd43ff33d1895ae
hxunz <guswl2843@gmail.com>
2023-03-24 00:10:49 +0900

    뒤에 있는 큰 수 찾기 해설 주석으로 추가

--- Javascript/programmers/Level2/뒤에있는큰수찾기.test.js
+++ Javascript/programmers/Level2/뒤에있는큰수찾기.test.js
@@ -21,10 +21,13 @@
 const solution = (numbers) => {
   const answer = new Array(numbers.length).fill(-1);
   const stack = [];
-  
+
   for (let i = 0; i < numbers.length; i++) {
+    //stack이 비어져있지 않고 현재 숫자보다 작은 숫자가 있는 경우 반복한다.
     while (stack.length && numbers[stack[stack.length - 1]] < numbers[i]) {
+      //스택에서 인덱스를 하나씩 꺼내서 해당 인덱스의 값을 numbers[i]로 바꿔서 뒤에 있는 큰 수를 찾는다.
       answer[stack.pop()] = numbers[i];
+      //이를 반복하면서 스택에 남아있는 인덱스들에 대해서도 뒤에 있는 큰 수를 찾아낸다. 
     }
     stack.push(i);
   }
